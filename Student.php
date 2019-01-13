4e77f24cbd8aa386c16dd1a1f1f487d11ba373da
jpancho <jpancho@sfu.ca>
2019-01-13 15:28:06 -0800

    Add mutators

--- Student.php
+++ Student.php
@@ -20,4 +20,14 @@ class Student
         $this->emails = array();
         $this->grades = array();
     }
+    
+    function add_email($which, $address)
+    {
+        $this->emails[$which] = $address;
+    }
+    
+    function add_grade($grade)
+    {
+        $this->grades[] = $grade;
+    }
 }
